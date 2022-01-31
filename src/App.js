c1bde8ce77b4f242ac57bece10a395cea5b51e27
Syed Zaid <syed.zaid525@gmail.com>
2022-01-31 18:00:59 +0530

    updated react-router-dom based on new versions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import "./App.css";
+import { Home } from "./views";
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import Two from "./views/Two";
 
-function App() {
+const App = () => {
+  // const dispatch = useDispatch();
+  // to check firebase auth state
+
+  // useEffect(() => {
+  //   // setInterval(async () => {
+  //   //   console.log(await (await KeepAliveServer()).data);
+  //   // }, [3000]);
+  //   const unsubscribe = auth.onAuthStateChanged(async (user) => {
+  //     if (user) {
+  //       const idTokenResult = await user.getIdTokenResult();
+  //       currentUser(idTokenResult.token)
+  //         .then((res) => {
+  //           dispatch({
+  //             type: "LOGGED_IN_USER",
+  //             payload: {
+  //               name: res.data.name,
+  //               email: res.data.email,
+  //               role: res.data.role,
+  //               token: idTokenResult.token,
+  //               _id: res.data._id,
+  //             },
+  //           });
+  //         })
+  //         .catch((err) => console.log(err));
+  //     }
+  //   });
+
+  //   // cleanup
+  //   return () => unsubscribe();
+  // }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to AK Creations testing</h1>
-      </header>
-    </div>
+    <Suspense
+    // fallback={
+    //   <div style={{ position: "fixed", left: "45vw", top: "45vh" }}>
+    //     __ PM __
+    //   </div>
+    // }
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/two" element={<Two />} />
+      </Routes>
+    </Suspense>
   );
-}
+};
 
 export default App;
---
+++ src/views/Two.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Two = () => {
+    return (
+        <div>
+            <h3>aslkjdsalksak</h3>
+        </div>
+    );
+};
+
+export default Two;
